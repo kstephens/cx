2917a079b20b28ba8c10eb789cca644f77b52f34
Kurt Stephens <git@kurtstephens.com>
2020-09-17 17:38:52 -0500

    cx --version

--- spec/cx_spec.rb
+++ spec/cx_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe Cx do
+RSpec.describe CX do
   it "has a version number" do
-    expect(Cx::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(CX::VERSION).not_to be nil
   end
 end
