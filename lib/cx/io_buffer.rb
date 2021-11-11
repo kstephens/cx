7452695a1b80323fb2a489dd0c48892a4313b3d1
Kurt Stephens <git@kurtstephens.com>
2021-11-11 14:06:55 -0600

    IOBuffer: handle objects that respond to :write.

--- lib/cx/io_buffer.rb
+++ lib/cx/io_buffer.rb
@@ -1,6 +1,10 @@
 module CX
   class IOBuffer
     def initialize out, term = "\n"
+      if out.respond_to?(:write) # e.g: IO, IOString
+        out_io = out
+        out = lambda{|x| out_io.write out}
+      end
       @out   = out
       @term  = term
       @buf   = String.new
@@ -8,10 +12,11 @@ module CX
     def call x
       @buf << x.to_str
       while i = @buf.index(@term)
-        pp @buf[r = 0 .. i]
+        # pp @buf[r = 0 .. i]
         @out.call @buf[r = 0 .. i]
         @buf[r] = ''
       end
+      nil
     end
     alias :<< :call
     alias :write :call
