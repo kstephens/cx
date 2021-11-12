b53291cbdfb97e20aa8d41fa6462d3d8bb630bdf
Kurt Stephens <git@kurtstephens.com>
2021-11-11 18:52:45 -0600

    Row.

--- lib/cx/table-new.rb
+++ lib/cx/table-new.rb
@@ -12,6 +12,7 @@ require 'cx/meta'
 require 'cx/inspect'
 require 'cx/column'
 require 'cx/header'
+require 'cx/row'
 require 'cx/xform/metatable'
 require 'cx/xform/pipeline'
 require 'cx/xform/strip'
@@ -142,129 +143,6 @@ class Table
 end
 
 
-class Row
-  include Enumerable
-  
-  attr_reader :header, :data, :header_version
-  attr_accessor :file_name, :line_number
-
-  def inspect mode = nil
-    case mode
-    when :super
-      super()
-    else
-      to_h.inspect
-    end
-  end
-
-  def self.[] x
-    case x
-    when self, nil
-      x
-    else
-      new(x)
-    end
-  end
-  
-  def initialize data = nil, header = nil
-    @data = data
-    @header = header
-  end
-
-  def new ; dup.deepen! ; end
-  def deepen!
-    @data = @data.dup
-    @meta = @meta && @meta.dup
-    self
-  end
-  
-  def _header= h
-    @header = h
-    self
-  end
-  
-  def each &blk
-    @header.each do |c|
-      yield c, _get(c)
-    end
-  end
-  
-  def size
-    @header.size
-  end
-
-  ###########################
-
-  def _get k
-    return nil unless k
-    case @data
-    when Hash
-      @data[k.to_sym]
-    else
-      @data[k.to_i]
-    end
-  end
-  
-  def _set k, v
-    raise TypeError unless k
-    case @data
-    when Hash
-      @data[k.to_sym] = v
-    else
-      @data[k.to_i] = v
-    end
-  end
-
-  def [] k
-    case k
-    when Column
-      _get(k)
-    when Symbol, String, Integer
-      _get(@header[k])
-    when nil
-      nil
-    else
-      raise TypeError
-    end
-  end
-
-  def []= k, v
-    case k
-    when Column
-      _set(k, v)
-    when Symbol, String, Integer
-      _set(@header[k], v)
-    when nil
-      nil
-    else
-      raise TypeError
-    end
-  end
-
-  def keys
-    @header
-  end
-
-  def values
-    @header.map{|c| _get(c)}
-  end
-  alias :to_a :values
-
-  def first ; _get(@header.first) ; end
-  def last  ; _get(@header.last)  ; end
-  
-  def to_h
-    h = { }
-    @header.each{|c| h[c.to_sym] = _get(c)}
-    h
-  end
-
-  def write out = nil
-    out ||= $stdout
-    each{|v| out.write(v.to_s)}
-    nil
-  end
-end
 
 ##############################
 
