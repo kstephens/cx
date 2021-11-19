90788908dd14fb86baffd7cd0576ec7fd1341e4e
Kurt Stephens <git@kurtstephens.com>
2021-11-19 17:59:44 -0600

    Type#assert_content.

--- lib/cx/test.rb
+++ lib/cx/test.rb
@@ -8,6 +8,7 @@ require 'cx/table'
 require 'cx/xform/header'
 require 'cx/xform/pipeline'
 require 'stringio'
+require 'pp'
 
 module CX
   module Test
@@ -85,7 +86,7 @@ module CX
       actual = run_pipeline(pipeline, opts || {})
       if expected
         if actual != expected
-          File.write("spec/last-pipeline.txt", actual)
+          File.write("spec/last-pipeline.actual", actual)
         end
         expect(actual) .to eq(expected)
       else
@@ -94,7 +95,59 @@ module CX
       end
       actual
     end
+
+    def assert_content name, actual_content
+      unless String === actual_content
+        strio = StringIO.new
+        PP.pp(actual_content, strio, 250)
+        actual_content = strio.string
+      end
+      expected_file = "spec/#{name}"
+      actual_file   = "#{expected_file}.actual"
+      diff_file     = "#{expected_file}.diff"
+      expected_content = File.read(expected_file) rescue nil
+      if actual_content != expected_content
+        File.write(expected_file, "") unless expected_content
+        File.write(actual_file, actual_content)
+        Kernel.system("set -x; diff -u '#{expected_file}' '#{actual_file}' | tee '#{diff_file}' >/dev/tty")
+        File.open("/dev/tty", "w+") do | tty |
+          tty.write " ### #{name} diff : Accept? [AY]: "
+          case tty.readline
+          when /^[ay]/i
+            File.write(expected_file, actual_content)
+            File.unlink(actual_file)
+            File.unlink(diff_file)
+          else
+            expect(actual_file) .to eq(expected_file)
+          end
+        end
+      end
+    rescue => exe
+      pp(exe)
+      binding.pry
+      :HERE
+    end
+
+    def assert_many actual, expected
+      unless expected == actual
+        msg = "actual = \n" + 
+          PP.pp(actual, '', 200)
+        # puts msg
+        File.write("spec/assert_many.actual", msg)
+        expect(actual) .to eq(expected)
+      end
+      actual
+    end
     
+    def stringify v
+      case v
+      when Time
+        v.iso8601(3)
+      else
+        v.to_s
+      end
+    end
+
     extend self
   end
 end
