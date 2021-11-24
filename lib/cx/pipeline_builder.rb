004312e211beb6eec105d8bc861ed29b4f0b7843
Kurt Stephens <git@kurtstephens.com>
2021-11-24 01:39:54 -0600

    PipelineBuilder: support sub-pipeline arguments.

--- lib/cx/pipeline_builder.rb
+++ lib/cx/pipeline_builder.rb
@@ -9,40 +9,86 @@ require 'cx/xform/pipeline'
 
 module CX
   class PipelineBuilder
-    attr_accessor :argv, :commands, :global, :pipeline, :factory
+    attr_accessor :argv, :pipeline, :global, :factory
 
     def parse! _argv
-      @argv = _argv.map(&:dup)
-
-      @commands = [ ]
-      command = [ ]
+      @argv = _argv.dup
 
       @global = Args.new.parse!(argv, no_args: true)
-      args = @global.args + [nil]
+      args = @global.args
       @global.args = []
-      args.each do | arg |
+      @pipeline = parse_pipeline! args
+      self
+    end
+
+    def parse_pipeline! args
+      parse_args! args, Pipeline.new
+    end
+    
+    def parse_args! args, pipeline
+      pending = [ ]
+      while arg = args.shift
         case arg
-        when '//', nil
-          commands << Args.new.parse!(command) unless command.empty?
-          command = [ ]
+        when nil
+        when '//'
+          pipeline << Command.new(pending) unless pending.empty?
+          pending = [ ]
+        when '{{'
+          pending << parse_pipeline!(args)
+        when '}}'
+          break
         else
-          command << arg
+          pending << arg
         end
       end
-      self
+      pipeline << Command.new(pending) unless pending.empty?
+      pipeline
     end
     
-    def build_pipeline
+    def build_xform
       @factory ||= CommandFactory.new.load!
-      @pipeline = Xform::Pipeline.new
-      commands.each do | args |
-        @pipeline >> build_xform(args)
-      end
-      self
+      @pipeline.build_xform(@factory)
     end
     
-    def build_xform argv
-      @factory.call(argv)
+    class Command < Struct.new(:argv, :args)
+      def initialize argv
+        self.argv = argv
+        self.args = Args.new.parse!(argv)
+      end
+      
+      def build_xform factory
+        args = Args.new.parse!(
+          argv.map do | arg |
+            case arg
+            when Pipeline, Command
+              arg.build_xform factory
+            else
+              arg
+            end
+          end
+        )
+        factory.call(args)
+      end
+    end
+
+    class Pipeline < Struct.new(:commands)
+      def initialize *args
+        super
+        self.commands ||= [ ]
+      end
+      
+      def << command
+        self.commands << command
+        self
+      end
+      
+      def build_xform factory
+        xform = Xform::Pipeline.new
+        commands.each do | command |
+          xform >> command.build_xform(factory)
+        end
+        xform
+      end
     end
   end
 end
--- spec/pipeline_builder_spec.rb
+++ spec/pipeline_builder_spec.rb
@@ -3,25 +3,85 @@ require 'cx/pipeline_builder'
 module CX
   RSpec.describe PipelineBuilder do
     subject() { PipelineBuilder.new.parse!(argv) }
-    let(:argv) {
-      "--gflg --gopt=abc124 cmd1 // cmd2 --flg2 arg2 // // cmd3 --flg3 --opt3=abc123 arg3 arg3".split(' ')
-    }
-    it "parses" do
-      expect(subject.argv) .to eq(argv)
-      expect(subject.global.opts) .to eq({:gflg=>true, :gopt=>"abc124"})
-      expect(subject.global.args) .to eq([])
-      expect(subject.commands.map(&:to_h))
-        .to eq(
-          [
-            {:argv=>["cmd1"], :args=>["cmd1"], :opts=>{}},
-            {:argv=>["cmd2", "--flg2", "arg2"],
-              :args=>["cmd2", "arg2"],
-              :opts=>{:flg2=>true}},
-            {:argv=>["cmd3", "--flg3", "--opt3=abc123", "arg3", "arg3"],
-              :args=>["cmd3", "arg3", "arg3"],
-              :opts=>{:flg3=>true, :opt3=>"abc123"}}
-          ]
-        )
+    describe 'basic pipeline' do
+      let(:argv) do
+        "--gflg --gopt=abc124 cmd1 // cmd2 --flg2 arg2 // // cmd3 --flg3 --opt3=abc123 arg3 arg3".split(' ')
+      end
+      it "parses" do
+        expect(subject.argv) .to eq(argv)
+        expect(subject.global.opts) .to eq({:gflg=>true, :gopt=>"abc124"})
+        expect(subject.global.args) .to eq([])
+        expect(subject.pipeline.commands.map(&:args).map(&:to_h))
+          .to eq(
+            [
+              {:argv=>["cmd1"], :args=>["cmd1"], :opts=>{}},
+              {:argv=>["cmd2", "--flg2", "arg2"],
+                :args=>["cmd2", "arg2"],
+                :opts=>{:flg2=>true}},
+              {:argv=>["cmd3", "--flg3", "--opt3=abc123", "arg3", "arg3"],
+                :args=>["cmd3", "arg3", "arg3"],
+                :opts=>{:flg3=>true, :opt3=>"abc123"}}
+            ]
+          )
+      end
+      describe "complex pipeline" do
+        let(:argv) do
+          "--gflg p1-c1 // p1-c2 --o1=123 {{ p2-c1 --f1 // p2-c3 --o2=234 }} p1-c2-a2 // // p1-c3".split(' ')
+        end
+        it "parses" do
+          expect(subject.argv) .to eq(argv)
+          expect(subject.global.opts) .to eq({:gflg=>true})
+          expect(subject.global.args) .to eq([])
+
+          cmds = subject.pipeline.commands
+          expect(cmds.map(&:class)).
+            to eq([
+              CX::PipelineBuilder::Command,
+              CX::PipelineBuilder::Command,
+              CX::PipelineBuilder::Command,
+            ])
+          
+          expect(cmds[0].args.to_h).
+            to eq({
+              :args=>["p1-c1"],
+              :argv=>["p1-c1"],
+              :opts=>{}})
+
+          expect(cmds[1].args.args.map(&:class)).
+            to eq([String, CX::PipelineBuilder::Pipeline, String])
+
+          expect(cmds[1].args.args[0]).
+            to eq("p1-c2")
+          
+          expect(cmds[1].args.args[1].commands.map(&:args).map(&:to_h)).
+            to eq([
+              {:args=>["p2-c1"],
+                :argv=>["p2-c1", "--f1"],
+                :opts=>{:f1=>true}
+              },
+              {:args=>["p2-c3"],
+                :argv=>["p2-c3", "--o2=234"],
+                :opts=>{:o2=>"234"}
+              }])
+
+          expect(cmds[1].args.args[2]).
+            to eq("p1-c2-a2")
+          
+          expect(cmds[2].args.to_h).
+            to eq({
+              :args=>["p1-c3"],
+              :argv=>["p1-c3"],
+              :opts=>{}})
+        end
+        
+        it "builds xform" do
+          subject.factory = lambda do | args |
+            args
+          end
+          actual = subject.build_xform
+          # pp(actual: actual)
+        end
+      end
     end
   end
 end
