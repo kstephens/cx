ff9bd6b90a81b1b47bc94225d17660f812ae5979
Kurt Stephens <git@kurtstephens.com>
2021-12-07 00:14:37 -0600

    JIRA table format.

---
+++ lib/cx/xform/jira.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'cx'
+require 'cx/xform'
+require 'cx/xform/record'
+
+
+# :COMMAND:
+# JiraOut:
+#   aliases: [ jira ]
+#   synopsis: Generate a Jira table lines.
+#   suffixes: [ .jira.txt ]
+#   args: []
+#   opts: {}
+#   examples:
+#     - 'cx in SOME.csv // -h // jira'
+
+module CX
+  module Xform
+    class JiraOut
+      include SelectColumns, OutputFormat, RecordOut
+      
+      def call input, env
+        title = opts[:title] # TODO
+        cols = column_args!(input).or_all!.columns
+        output = make_output
+        if include_header?
+          output << make_row(cols, "||")
+        end
+        input.each do | row |
+          output << make_row(row.vals(cols), "|")
+        end
+        output
+      end
+
+      def make_row row, sep
+        [ sep.dup << row.map{|c| escape(c)}.join(sep) << sep << "\n" ]
+      end
+      
+      def escape s
+        s.to_s.gsub(/[|]/, "\\|")
+      end
+    end
+  end
+end
+
