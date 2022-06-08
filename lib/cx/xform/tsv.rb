5257ac80222e39f14939d21eee685fb25dcc6c54
Kurt Stephens <git@kurtstephens.com>
2022-06-08 13:11:52 -0500

    TSV format.

---
+++ lib/cx/xform/tsv.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'cx'
+require 'cx/xform'
+require 'cx/xform/csv'
+
+# :COMMAND:
+# TsvIn:
+#   aliases:
+#   synopsis: Parses TSV lines.
+#   suffixes: [ .tsv ]
+#   inverse: [ 'tsv-' ]
+#   args: []
+#   opts: {}
+
+# :COMMAND:
+# TsvOut:
+#   aliases:
+#   synopsis: Generates TSV lines.
+#   suffixes: [ .tsv ]
+#   inverse: [ '-tsv' ]
+#   args: []
+#   opts: {}
+#   examples:
+#     - cx in SOME.csv // tsv-
+
+module CX
+  module Xform
+    class TsvIn < CsvIn
+      def call input, env
+        opts[:separator] ||= "\x09"
+        super
+      end
+    end
+    
+    class TsvOut < CsvOut
+      def call input, env
+        opts[:separator] ||= "\x09"
+        super
+      end
+    end
+  end
+end
+
