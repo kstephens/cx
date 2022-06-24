38f0b44601da735aee0e5588ceef612b07243d53
Kurt Stephens <git@kurtstephens.com>
2022-06-23 19:32:39 -0500

    SqlOut: support sql_type, sql_default.

--- lib/cx/xform/sql.rb
+++ lib/cx/xform/sql.rb
@@ -9,6 +9,7 @@ require 'cx/xform/record'
 # SqlOut:
 #   aliases: sql-
 #   synopsis: Generates CSV lines.
+#   has_column_args: true
 #   args: []
 #   opts:
 #     table=: Table name.
@@ -22,27 +23,30 @@ require 'cx/xform/record'
 #   examples:
 #     - cx in SOME.csv // -h // parse // sql- --table=SOME_TABLE --create
 #     - cx in SOME.csv // -h // parse // sql- --table=SOME_TABLE --insert
+#     - "cx in SOME.csv // -h // parse // cut a b c // parse // meta= 'a:sql_type=VARCHAR(5)' // sql- --table=SOME_TABLE a b 'c:type=NUMERIC;default=-1' --create --temporary"
+#     - "cx in SOME.csv // -h // cut a b c // parse // meta= 'a:sql_type=VARCHAR(5)' // sql- --table=SOME_TABLE --insert"
 #
 
 module CX
   module Xform
     class SqlOut
-      include OutputFormat, RecordOutBase
-      attr_reader :input, :header, :output, :table
+      include SelectColumns, OutputFormat, RecordOutBase
+      attr_reader :input, :columns, :output, :table
 
+      # TODO: handle dialects: Postgres, Oracle, Sqlite, etc.
       def call input, env
         @input = input
-        @header = input.header
+        @columns = column_args!(input).wildcards!.or_all!
         @output = make_output
-        @table = opts[:table] || @header.meta.name or raise_ "unspecifed table name"
+        @table = opts[:table] || input.header.meta.name or raise_ "unspecifed table name"
         @varchar_size = opts.fetch(:varchar_size, 255)
         
+        @columns.each{|ca| sql_type(ca)}
+
         do_actions!
 
         env[:content_type] = 'text/plain' # application/x-sql ?
-        result = @output
-        @input = @header = @output = @table = nil
-        result
+        @output.tap{|_| @input = @columns = @output = @table = nil }
       end
 
       def do_actions!
@@ -65,37 +69,43 @@ module CX
       end
 
       def transaction!
-        self <<<<"END"
+        self << <<"END"
 START TRANSACTION;
 END
       end
 
       def commit!
-        self <<<<"END"
+        self << <<"END"
 COMMIT;
 END
       end
 
       def rollback!
-        self <<<<"END"
+        self << <<"END"
 ROLLBACK;
 END
       end
   
       def create!
-        self <<<<"END"
+        self << <<"END"
 CREATE #{opts[:temporary] && "TEMPORARY "}TABLE #{sql_identifer(table)}
 (
-#{header.map{|c| "  " + sql_column_def(c)} * ",\n"}
+#{@columns.map{|ca| "  " + sql_column_def(ca)} * ",\n"}
 );
 END
       end
 
-      def sql_column_def col
-        "#{col.name_} #{type_to_sql_type col}"
+      def sql_column_def col_arg
+        col = col_arg.column
+        sql = "#{col.name_} #{col_arg.opts[:type]}"
+        if default_value = col_arg.opts[:default]
+          sql += " DEFAULT #{sql_val(default_value, col_arg)}"
+        end
+        sql
       end
 
-      def type_to_sql_type col
+      def sql_type col_arg
+        col = col_arg.column
         case cls = col.meta.type_
         when Symbol, nil
           cls = String
@@ -109,7 +119,8 @@ END
           Object  => "TEXT",
         }
         # cls = Typing.first_superclass(sql_types.keys, type)
-        sql_types[cls] || sql_types[Object]
+        col_arg.opts[:default] ||= col.meta.opts[:sql_default]
+        col_arg.opts[:type]    ||= col.meta.opts[:sql_type] || sql_types[cls] || sql_types[Object]
       end
 
       def << str
@@ -121,7 +132,7 @@ END
     def insert!
       sql_insert_into = <<"END"
 INSERT INTO #{sql_identifer(table)}
-  #{sql_columns(header)}
+  #{sql_columns(@columns)}
 VALUES
 END
       sep = "  "
@@ -139,8 +150,8 @@ END
       end
     end
 
-    def sql_columns cols
-      sql_list cols.map{|c| sql_identifer(c.name_)} # ??? may require quoting
+    def sql_columns col_args
+      sql_list col_args.map{|ca| sql_identifer(ca.column.name_)} # ??? may require quoting
     end
 
     def sql_identifer name
@@ -148,18 +159,18 @@ END
     end
 
     def sql_row row
-      sql_list @header.map{|c| sql_val(row[c.to_i])}
+      sql_list @columns.map{|ca| sql_val(row[ca.column], ca)}
     end
 
     def sql_list arr
       String.new << '(' << (arr.map(&:to_s) * ', ') << ')'
     end
 
-    def sql_val v # TODO: proper string escape.
-      case v
-      when nil
-        "NULL"
-      when String, Symbol
+    # TODO: proper string escape.
+    def sql_val v, col_arg
+      return "NULL" if v.nil?
+      case col_arg.opts[:type]
+      when /^(TEXT|CHAR|VARCHAR)/i # ??? do more?
         # ??? good enough?
         v = v.to_s.
           inspect.
